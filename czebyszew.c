33a6bcae9819b6c5544e08614a5c951388a93694
Weronika Zbierowska <hello.weri@vp.pl>
2022-01-23 15:05:23 +0100

    ostateczna wersja czebyszewa

--- czebyszew.c
+++ czebyszew.c
@@ -56,32 +56,32 @@ void make_spl(points_t * pts, spline_t * spl)
 	matrix_t *eqs= NULL;
 	double *x = pts->x;
 	double *y = pts->y;
-	double	a = x[0];
+	double	a = x[0];				// a,b - przedział aproksymacji
 	double	b = x[pts->n - 1];
 	int i, j, k;
-	int nb = pts->n - 3 > 10 ? 10 : pts->n - 3;
+	int nb = pts->n - 3 > 10 ? 10 : pts->n - 3;	// rozmiar bazy
   	char *nbEnv = getenv("APPROX_BASE_SIZE");
 
 	if(nbEnv != NULL && atoi(nbEnv) > 0)
 		nb = atoi(nbEnv);
 	
-	eqs = make_matrix(nb, nb + 1);
+	eqs = make_matrix(nb, nb + 1);			// utworzenie macierzy nb x nb+1
 
 	for(j = 0; j < nb; j++)
 	{
 		for(i = 0; i < nb; i++)
-			for (k = 0; k < pts->n; k++)
-				add_to_entry_matrix(eqs, j, i, fi(i, x[k]) * fi(j, x[k]));
+			for (k = 0; k < pts->n; k++)	// suma pts->n wyrażeń
+				add_to_entry_matrix(eqs, j, i, fi(i, x[k]) * fi(j, x[k]));	// zapis D^TD do macierzy eqs
 
-		for(k = 0; k < pts->n; k++)
-			add_to_entry_matrix(eqs, j, nb, y[k] * fi(j, x[k]));
+		for(k = 0; k < pts->n; k++)		// suma pts->n wyrażeń
+			add_to_entry_matrix(eqs, j, nb, y[k] * fi(j, x[k]));	// zapis D^Tf do nb-tej kolumny macierzy eqs
 	}
 
 #ifdef DEBUG
 	write_matrix(eqs, stdout);
 #endif
 
-	if(piv_ge_solver(eqs))
+	if(piv_ge_solver(eqs))		// rozwiązanie równania macierzowego
 	{
 		spl->n = 0;
 		return;
@@ -93,17 +93,17 @@ void make_spl(points_t * pts, spline_t * spl)
 
 	if(alloc_spl(spl, nb) == 0)
 	{
-		for(i = 0; i < spl->n; i++)
+		for(i = 0; i < spl->n; i++)	// zapis wartości f, f1, f2, f3 do struktury splines
 		{
-			double xx = spl->x[i] = a + i * (b - a) / (spl->n - 1);
-			xx += 10.0 * DBL_EPSILON;	// zabezpieczenie przed ulokowaniem punktu w poprzednim przedziale
+			double xx = spl->x[i] = a + i * (b - a) / (spl->n - 1);		// granica jednego z nb-1 podprzedziałów
+			xx += 10.0 * DBL_EPSILON;	// zabezpieczenie przed ulokowaniem punktu w poprzednim podprzedziale
 			spl->f[i] = 0;
 			spl->f1[i] = 0;
 			spl->f2[i] = 0;
 			spl->f3[i] = 0;
 			for(k = 0; k < nb; k++)
 			{
-				double ck = get_entry_matrix(eqs, k, nb);
+				double ck = get_entry_matrix(eqs, k, nb);	// współczynnik a_0, a_1, ..., a_k
 				spl->f[i] += ck * fi(k, xx);
 				spl->f1[i] += ck * dfi(k, xx);
 				spl->f2[i] += ck * d2fi(k, xx);
@@ -112,5 +112,5 @@ void make_spl(points_t * pts, spline_t * spl)
 		}
 	}
 
-	free_matrix(eqs);
+	free_matrix(eqs);		// zwolnienie pamięci dla macierzy eqs
 }
